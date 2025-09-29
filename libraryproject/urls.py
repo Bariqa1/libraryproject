cccbfc94ba9625130b13ef32772771d729da8d2a
bariqa <xaltamimi77@gmail.com>
2025-09-29 15:59:43 +0300

    Updated project with urls and templates

--- libraryproject/urls.py
+++ libraryproject/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+#from apps.bookmodule import views as views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('', views.index),
+    # path('index2/<int:val1>/', views.index2),
+    path('books/', include("apps.bookmodule.urls")), 
+    path('users/', include("apps.usermodule.urls")),
 ]
