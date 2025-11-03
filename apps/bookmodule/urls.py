3a780430260c25568f9630f3d14f54ff836e2bcf
bariqa <xaltamimi77@gmail.com>
2025-11-03 11:19:13 +0300

    feat: Add new features and general updates

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -1,8 +1,30 @@
+# apps/bookmodule/urls.py
 
 from django.urls import path
 from . import views 
+
 urlpatterns = [ 
-path('', views.index), 
-path('index2/<int:val1>/', views.index2),
-path('<int:bookId>', views.viewbook),
+    path('', views.index, name="books.index"), 
+    
+    path('list_books/', views.list_books, name="books.list_books"), 
+    
+    path('<int:bookId>/', views.viewbook, name="books.view_one_book"), 
+    
+    path('aboutus/', views.aboutus, name="books.aboutus"), 
+    
+    path('html5/links', views.links_page_view, name='html5_links'),
+
+    path('html5/text/formatting', views.text_formatting_view, name='text_formatting'),
+
+    path('html5/listing', views.listing_view, name='html5_listing'),
+
+    path('html5/tables', views.tables_view, name='html5_tables'),
+    
+    path('search', views.book_search_view, name='book_search'),
+
+    path('simple/query', views.simple_query, name='simple_book_query'),
+    
+    path('complex/query', views.complex_query, name='complex_book_query'),
 ]
+
+
--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -1,20 +1,89 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse 
-def index(request):  
-    name = request.GET.get("name") or "world!" 
-    return render(request, "bookmodule/index.html" , {"name": name})   
+from .models import Book
 
-def index2(request, val1 = 0):   
-#add the view function (index2) 
-    return HttpResponse("value1 = "+str(val1))
+BOOKS_DATA = {
+    1: {'id': 1, 'title': 'Internet & World Wide Web How to Program', 'author': 'author name', 'description': 'Description for Book 1.', 'image_file': 'book1.jpg'},
+    2: {'id': 2, 'title': 'C++ How to Program, Late Objects Version', 'author': 'author name', 'description': 'Description for Book 2.', 'image_file': 'book2.jpg'},
+    3: {'id': 3, 'title': 'Images in Another Folder', 'author': 'author name', 'description': 'Description for Book 3.', 'image_file': 'book3.jpg'}
+}
+BOOK_123 = {'id':123, 'title':'Continuous Delivery', 'author':'J. Humble and D. Farley', 'description': 'CD description.', 'image_file': 'book1.jpg'}
+BOOK_456 = {'id':456, 'title':'Secrets of Reverse Engineering', 'author':'E. Eilam', 'description': 'SRE description.', 'image_file': 'book2.jpg'}
+
+BOOKS_DATA[BOOK_123['id']] = BOOK_123
+BOOKS_DATA[BOOK_456['id']] = BOOK_456
+
+
+def index(request): 
+    return render(request, "bookmodule/index.html") 
+
+def list_books(request): 
+    return render(request, 'bookmodule/list_books.html') 
 
 def viewbook(request, bookId): 
-# assume that we have the following books somewhere (e.g. database) 
-    book1 = {'id':123, 'title':'Continuous Delivery', 'author':'J. Humble and D. Farley'} 
-    book2 = {'id':456, 'title':'Secrets of Reverse Engineering', 'author':'E. Eilam'} 
-    targetBook = None 
-    if book1['id'] == bookId: targetBook = book1 
-    if book2['id'] == bookId: targetBook = book2 
-    context = {'book':targetBook} # book is the variable name accessible by the template 
-    return render(request, 'bookmodule/show.html', context)
+    targetBook = BOOKS_DATA.get(bookId)
+
+    if targetBook is None:
+        targetBook = {'title': 'Book Not Found', 'author': 'N/A', 'description': 'N/A', 'image_file': 'default.jpg'}
+        
+    context = {'book': targetBook, 'book_id': bookId}
+    return render(request, 'bookmodule/one_book.html', context) 
+
+def aboutus(request): 
+    return render(request, 'bookmodule/aboutus.html')
+
+def links_page_view(request):
+    return render(request, 'bookmodule/links_page.html')
+
+def text_formatting_view(request):
+    return render(request, 'bookmodule/formatting_page.html')
+
+def listing_view(request):
+    return render(request, 'bookmodule/listing_page.html')
+
+def tables_view(request):
+    return render(request, 'bookmodule/tables_page.html')
+
+
+def __getBooksList():
+    book1 = {'id':12344321, 'title':'Continuous Delivery', 'author':'J.Humble and D. Farley'}
+    book2 = {'id':56788765,'title':'Reversing: Secrets of Reverse Engineering', 'author':'E. Eilam'}
+    book3 = {'id':43211234, 'title':'The Hundred-Page Machine Learning Book', 'author':'Andriy Burkov'}
+    return [book1, book2, book3]
+
+def book_search_view(request):
+    if request.method == "POST":
+        string = request.POST.get('keyword', '').lower()
+        isTitle = request.POST.get('option1')  
+        isAuthor = request.POST.get('option2') 
+
+        books = __getBooksList()
+        newBooks = []
+        for item in books:
+            contained = False
+            
+            if isTitle and string in item['title'].lower(): 
+                contained = True
+ 
+            if not contained and isAuthor and string in item['author'].lower():
+                contained = True
+                
+            if contained: 
+                newBooks.append(item)
+        
+        return render(request, 'bookmodule/bookList.html', {'books': newBooks, 'keyword': string})
+   
+    return render(request, 'bookmodule/bookSearchForm.html')
+
+
+def simple_query(request): 
+    mybooks=Book.objects.filter(title__icontains='and') # <- multiple objects 
+    return render(request, 'bookmodule/bookList.html', {'books':mybooks}) 
+
+
+def complex_query(request): 
+    mybooks=books=Book.objects.filter(author__isnull = False).filter(title__icontains='and').filter(edition__gte = 2).exclude(price__lte = 100)[:10] 
+    if len(mybooks)>=1: 
+       return render(request, 'bookmodule/bookList.html', {'books':mybooks}) 
+    else: 
+      return render(request, 'bookmodule/index.html') 
